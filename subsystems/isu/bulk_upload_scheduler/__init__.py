de54ad8e7a9d974cce1ce7a609c23cc212ebfc84
Martin Landa <landa.martin@gmail.com>
2026-02-05 10:50:14 +0100

    initial code layout introduced (#29)

---
+++ subsystems/isu/bulk_upload_scheduler/__init__.py
@@ -0,0 +1,8 @@
+class BulkUploadScheduler:
+    """Bulk Upload Scheduler periodically retrieves files from S3
+    buckets, FTP/SFTP servers, shared cloud drives, or other
+    standardized storage endpoints exposed by external systems.
+    """
+
+    def __init__(self):
+        pass
