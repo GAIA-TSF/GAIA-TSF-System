d5ab1f42eecb8a2cd0e25d1e9f58a3a741b9325a
pesekon2 <17025405+pesekon2@users.noreply.github.com>
2026-02-05 10:00:38 +0000

    Apply Ruff formatting and fixes

--- subsystems/isu/__init__.py
+++ subsystems/isu/__init__.py
@@ -5,17 +5,19 @@ from isu.data_extraction import DataExtraction
 
 from qcl.logger import Logger
 
+
 class InSituDataUploader:
     """In-Situ Data Uploader sub-system is responsible for collecting
     and securely transmitting field-acquired data from different
     ground-based sensor technologies to the central data
     pre-processing module.
     """
-    id = "ISU"
+
+    id = 'ISU'
 
     def __init__(self):
         self.logger = Logger(subsystem=self.id)
-        self.logger.debug("initialized")
+        self.logger.debug('initialized')
 
         self.manual_file_loader = ManualFileLoader()
         self.bulk_upload_scheduler = BulkUploadScheduler()
