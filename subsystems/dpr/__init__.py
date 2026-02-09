82e33511bbb99ecccd87052f4ef8f18ddbb962bf
Martin Landa <landa.martin@gmail.com>
2026-02-09 18:53:43 +0100

    initial DPR code layout introduced (#54)
    * initial dpr code layout introduced
    
    * Apply Ruff formatting and fixes (#55)
    
    Co-authored-by: landam <5683186+landam@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: landam <5683186+landam@users.noreply.github.com>

---
+++ subsystems/dpr/__init__.py
@@ -0,0 +1,27 @@
+from dpr.preprocessing_pipelines import PreprocessingPipelines
+from dpr.data_analysis_pipelines import DataAnalysisPipelines
+from dpr.metadata_processor import MetadataProcessor
+
+from qcl.logger import Logger
+
+
+class DataProcessing:
+    """Data Processing sub-system serves as the central refinement
+    engine of the architecture, responsible for transforming raw inputs
+    into standardized, analysis-ready information products. It encompasses
+    three primary functional areas: metadata management, data
+    preprocessing, and advanced data analysis. The sub-system ensures that
+    all ingested data—whether from satellite imagery or in-situ
+    sensors—are accurately described, geometrically and atmospherically
+    corrected, and derived into meaningful indicators before storage.
+    """
+
+    id = 'DPR'
+
+    def __init__(self):
+        self.logger = Logger(subsystem=self.id)
+        self.logger.debug('initialized')
+
+        self.preprocessing_pipelines = PreprocessingPipelines()
+        self.data_analysis_pipelines = DataAnalysisPipelines()
+        self.metadata_processor = MetadataProcessor()
