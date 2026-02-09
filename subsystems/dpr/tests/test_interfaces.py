b015fd5b75db6dc6d92fa44d04b105a40a9739d9
Martin Landa <landa.martin@gmail.com>
2026-02-09 18:19:13 +0100

    initial dpr code layout introduced

---
+++ subsystems/dpr/tests/test_interfaces.py
@@ -0,0 +1,21 @@
+class TestInterfaces:
+    def test_IF1_001(self):
+        """Test IF1 DataProcessor.
+
+        Example of integration test.
+        """
+        pass
+
+    def test_IF1_002(self):
+        """Test IF1 DataProcessor.
+
+        Another example of integration test.
+        """
+        pass
+
+    def test_IF2_001(self):
+        """Test IF2 QualityControlLogging Layer.
+
+        Example of integration test.
+        """
+        pass
