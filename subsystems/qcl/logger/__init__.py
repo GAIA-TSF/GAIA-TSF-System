6e9d5e231b8d2d7332d02a42bdeb8bedd87c347a
landam <5683186+landam@users.noreply.github.com>
2026-02-17 14:15:26 +0000

    Apply Ruff formatting and fixes

--- subsystems/qcl/logger/__init__.py
+++ subsystems/qcl/logger/__init__.py
@@ -13,7 +13,7 @@ class Logger:
 
             handler = logging.StreamHandler(sys.stdout)
             formatter = logging.Formatter(
-                "%(asctime)s - %(name)s - %(subsystem)s - %(levelname)s - %(message)s"
+                '%(asctime)s - %(name)s - %(subsystem)s - %(levelname)s - %(message)s'
             )
             handler.setFormatter(formatter)
             base_logger.addHandler(handler)
