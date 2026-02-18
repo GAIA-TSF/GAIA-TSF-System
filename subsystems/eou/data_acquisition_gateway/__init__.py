cb5842142daf219c31fdfa9c2558d309ed5e184b
Martin Landa <landa.martin@gmail.com>
2026-02-18 19:22:32 +0100

    Integrate eodag as backend for Data Acquisition Gateway module (#9)
    * search and download methods implemented
    ---------
    
    Co-authored-by: bouceto <tom.boucek@seznam.cz>
    Co-authored-by: Ondrej Pesek <pesej.ondrek@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- subsystems/eou/data_acquisition_gateway/__init__.py
+++ subsystems/eou/data_acquisition_gateway/__init__.py
@@ -1,7 +1,61 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from eodag.api.search_result import SearchResult
+    from eodag_cube.api.product._product import EOProduct
+
+
 class DataAcquisitionGateway:
     """Data Acquisition Gateway module serves as the automated
     ingestion engine for the sub-system.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, backend: str = 'eodag'):
+        if backend == 'eodag':
+            from eou.data_acquisition_gateway.eodag_backend import (
+                EODAGDataAcquisitionBackend as DataAcquisitionBackend,
+            )
+        else:
+            raise RuntimeError(f'Unsupported data acquisition backend: {backend}')
+
+        self._backend = DataAcquisitionBackend()
+
+    def search(
+        self, provider: str, start: str, end: str, geom: list[float], **kwargs
+    ) -> SearchResult:
+        """Search for data products that match the specified criteria
+        across supported providers using selected data acquisition
+        backend.
+
+        :param str provider: the provider to be used
+        :param str start: start date to be used for temporal filter
+        :param str end: end date to be used for temporal filer
+        :param list(float) geom: a bounding box as list of float: [lonmin, latmin, lonmax, latmax]
+
+        For other arguments check the backend:
+         - eodag: https://eodag.readthedocs.io/en/stable/api_reference/core.html#eodag.api.core.EODataAccessGateway.search
+
+        :return: a collection of EO products matching the criteria
+        :rtype: SearchResult
+        """
+        return self._backend.search(provider, start, end, geom, **kwargs)
+
+    def download(self, product: EOProduct, quicklook: bool = False, **kwargs) -> str:
+        """Download selected data product using selected data
+        acquisition backend.
+
+        :param EOProduct product: EO product to be downloaded
+        :param bool quicklook: If True, only download the preview image
+        :return: a path to the download data
+        :rtype: str
+        """
+        return self._backend.download(product, quicklook=quicklook, **kwargs)
+
+    def set_config(self, config_file: str) -> None:
+        """Set configuration options for data acquisition backend.
+
+        :param str config_file: Configuration file path
+        :return: None
+        """
+        self._backend.set_config(config_file)
--- subsystems/eou/tests/test_modules.py
+++ subsystems/eou/tests/test_modules.py
@@ -1,7 +1,40 @@
 from pathlib import Path
 
+from osgeo import gdal
+
+gdal.UseExceptions()
+
+from eou.data_acquisition_gateway import DataAcquisitionGateway
+
+
+def load_geom(file_path):
+    ds = gdal.OpenEx(file_path, gdal.OF_VECTOR)
+    layer = ds.GetLayer(0)
+
+    srs = layer.GetSpatialRef()
+    srs.AutoIdentifyEPSG()
+    auth = srs.GetAuthorityName(None)
+    code = srs.GetAuthorityCode(None)
+    if auth != 'EPSG' or code != '4326':
+        raise RuntimeError(f'Unsupported CRS: {auth}:{code}')
+    lonmin, lonmax, latmin, latmax = layer.GetExtent()
+    ds = None
+
+    return [lonmin, latmin, lonmax, latmax]
+
 
 class TestModules:
+    search_filter = {
+        'provider': 'cop_dataspace',
+        'start': '2026-01-01',
+        'end': '2026-01-29',
+        'productType': 'S2_MSI_L2A',
+    }
+
+    @staticmethod
+    def _get_data_path(filename):
+        return str(Path(__file__).parent / 'sample_data' / filename)
+
     def test_ManualFileLoader_001(self):
         """Test ManualFileLoader module.
 
@@ -10,9 +43,7 @@ class TestModules:
         from eou.manual_file_loader import ManualFileLoader
 
         module = ManualFileLoader()
-        result = module.check_file_validity(
-            str(Path(__file__).parent / 'sample_data' / 'ENMAP01_sample.tif')
-        )
+        result = module.check_file_validity(self._get_data_path('ENMAP01_sample.tif'))
 
         assert result['valid'] is True and result['driver'] == 'GTiff'
         assert len(result['errors']) < 1
@@ -21,16 +52,43 @@ class TestModules:
     def test_DataAcquisitionGateway_001(self):
         """Test DataAcquisitionGateway module.
 
-        Example of unit test.
+        Test search capability using default backend (eodag).
         """
-        pass
+        from eodag.api.search_result import SearchResult
+
+        module = DataAcquisitionGateway()
+
+        result = module.search(
+            geom=load_geom(self._get_data_path('area_intervencao.kmz')),
+            **self.search_filter,
+        )
+
+        assert isinstance(result, SearchResult)
+        assert len(result) > 0
+        assert result[0].product_type == self.search_filter['productType']
 
     def test_DataAcquisitionGateway_002(self):
         """Test DataAcquisitionGateway module.
 
-        Another example of unit test.
+        Test download capability using default backend (eodag).
         """
-        pass
+        config_file = 'eou/tests/eodag_config.yml'
+
+        module = DataAcquisitionGateway()
+
+        results = module.search(
+            geom=load_geom(self._get_data_path('area_intervencao.kmz')),
+            **self.search_filter,
+        )
+
+        assert len(results) > 0
+
+        module.set_config(config_file)
+        ql_path = module.download(results[0], quicklook=True)
+
+        assert isinstance(ql_path, str)
+        assert Path(ql_path).exists()
+        assert Path(ql_path).stat().st_size > 0
 
     def test_DataExtraction_001(self):
         """Test DataExtraction module.
