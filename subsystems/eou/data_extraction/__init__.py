2a6fe6e20b4c1e53be598ee53e9305f6c13f2659
Martin Landa <landa.martin@gmail.com>
2026-02-23 18:53:22 +0100

    make ruff happy

--- subsystems/eou/data_extraction/__init__.py
+++ subsystems/eou/data_extraction/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Dict, Any
+    from .metadata import StacItemFactory
 
 from .metadata import RasterDataset, StacItemFactory
 
