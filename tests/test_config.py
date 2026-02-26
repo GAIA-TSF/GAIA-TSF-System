908d98399fbb79e3e70589bbfdbff2dba745c87d
Martin Landa <landa.martin@gmail.com>
2026-02-26 17:18:14 +0100

    move ./projects to ./tests/

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,7 +6,7 @@ class TestConfig:
         from lib.config import ConfigReader
 
         config = ConfigReader(
-            Path(__file__).parent.parent / 'projects' / 'jagersfontein.yml'
+            Path(__file__).parent / 'projects' / 'jagersfontein.yml'
         )
 
         assert config['project']['name'] == 'Jagersfontein'
