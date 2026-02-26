5a260e7c4246a648cdcd7ab2c2fc61c5f385a35f
bouceto <tom.boucek@seznam.cz>
2026-02-26 15:47:54 +0100

    config reader implementation

---
+++ tests/test_config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+class TestConfig:
+    def test_config_001(self):
+        from lib.config import ConfigReader
+
+        config = ConfigReader(Path(__file__).parent.parent / "projects" / "jagersfontein.yml")
+
+        assert config["project"]["name"] == "Jagersfontein"
