68d29bf85a58a3a7d2e82e74662f148067a3e812
Martin Landa <landa.martin@gmail.com>
2026-02-27 12:15:24 +0100

    Introduce YamlValidator and ProjectConfigReader (#107)
    * introduce YamlValidator and ProjectConfigReader
    
    * Apply Ruff formatting and fixes (#108)
    
    Co-authored-by: landam <5683186+landam@users.noreply.github.com>
    
    * Fix docstring: no exception raised at this moment
    
    * downgrade shapely
    
    * no need to set errors in tests
    
    * Apply Ruff formatting and fixes (#109)
    
    Co-authored-by: landam <5683186+landam@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: landam <5683186+landam@users.noreply.github.com>

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,10 +1,35 @@
 from pathlib import Path
 
-from lib.config import ConfigReader
+from lib.config import ConfigReader, ProjectConfigReader
 
 
 class TestConfig:
     def test_config_001(self):
+        """Process sample project file by ConfigReader and check project/name option."""
         config = ConfigReader(Path(__file__).parent / 'projects' / 'jagersfontein.yml')
 
         assert config['project']['name'] == 'Jagersfontein'
+
+    def test_config_002(self):
+        """Process sample project config file by ProjectConfigReader."""
+        config = ProjectConfigReader(
+            Path(__file__).parent / 'projects' / 'jagersfontein.yml'
+        )
+
+        assert config.is_valid() is True
+
+        # delete required option
+        config = ProjectConfigReader(
+            Path(__file__).parent / 'projects' / 'jagersfontein.yml'
+        )
+        del config['project']['name']
+        config.validate(dict(config))  # re-validate config after modification
+        assert config.is_valid() is False
+
+        # make WKT invalid
+        config = ProjectConfigReader(
+            Path(__file__).parent / 'projects' / 'jagersfontein.yml'
+        )
+        config['project']['aoi']['geom'] = 'X'
+        config.validate(dict(config))  # re-validate config after modification
+        assert config.is_valid() is False
