e716ca2f17de91a9c5e8f105cba64339e9fffda6
Tomáš Bouček <48289035+bouceto@users.noreply.github.com>
2026-03-03 11:36:43 +0100

    test eou subsystem to download S1 data (#116)
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Martin Landa <landa.martin@gmail.com>

---
+++ subsystems/dpr/tests/test_sentinel1.py
@@ -0,0 +1,51 @@
+import sys
+from pathlib import Path
+
+
+# to be removed when https://github.com/GAIA-TSF/GAIA-TSF-System/issues/97 is solved
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent.parent))
+from lib.config import ProjectConfigReader
+from eou.data_acquisition_gateway import DataAcquisitionGateway
+
+
+class TestSentinel1Workflow:
+    search_filter = {
+        'provider': 'cop_dataspace',
+        'start': '2026-01-01',
+        'end': '2026-01-29',
+        'productType': 'S1_SAR_SLC',
+    }
+
+    def test_download(self):
+        """Test EOU Data Acquisition Gateway to download Sentinel-1 data."""
+        config = ProjectConfigReader(
+            str(
+                Path(__file__).parent.parent.parent.parent
+                / 'tests'
+                / 'projects'
+                / 'jagersfontein.yml'
+            )
+        )
+
+        assert config.is_valid() is True
+
+        module = DataAcquisitionGateway()
+        results = module.search(
+            geom=config['project']['aoi']['geom'],
+            **self.search_filter,
+        )
+
+        assert len(results) > 0
+
+        config_eodag = str(
+            Path(__file__).parent.parent.parent / 'eou' / 'tests' / 'eodag_config.yml'
+        )
+        module.set_config(config_eodag)
+
+        ql_path = None
+        try:
+            ql_path = module.download(results[0], quicklook=True)
+            assert Path(ql_path).exists()
+        finally:
+            if ql_path and Path(ql_path).exists():
+                Path(ql_path).unlink()
