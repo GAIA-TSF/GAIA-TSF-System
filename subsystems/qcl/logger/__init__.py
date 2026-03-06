6b90dcf970bd074d65caef597bad1945a05df3f8
landam <5683186+landam@users.noreply.github.com>
2026-03-06 20:12:11 +0000

    Apply Ruff formatting and fixes

--- subsystems/qcl/logger/__init__.py
+++ subsystems/qcl/logger/__init__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 # TBD: remove when root path defined
 # https://github.com/GAIA-TSF/GAIA-TSF-System/issues/145
 import sys
+
 sys.path.insert(0, str(Path(__file__).parent.parent.parent.parent))
 from lib.config import SettingsReader
 
