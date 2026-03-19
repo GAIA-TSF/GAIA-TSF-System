28fd65a7dc2aeadbafc25073d3a796f0a2359ba4
Ondrej Pesek <pesej.ondrek@gmail.com>
2026-03-19 15:25:05 +0100

    Exporter and integration test: initial commit (#164)
    ---
    Co-authored-by: Martin Landa <landa.martin@gmail.com>

--- subsystems/eou/__init__.py
+++ subsystems/eou/__init__.py
@@ -1,6 +1,5 @@
 from subsystems.eou.manual_file_loader import ManualFileLoader
 from subsystems.eou.data_acquisition_gateway import DataAcquisitionGateway
-from subsystems.eou.data_extraction import DataExtraction
 
 from subsystems.qcl.logger import Logger
 
@@ -18,4 +17,3 @@ class EarthObservationDataUploader:
 
         self.manual_file_loader = ManualFileLoader()
         self.data_acquisition_gateway = DataAcquisitionGateway()
-        self.data_extraction = DataExtraction()
