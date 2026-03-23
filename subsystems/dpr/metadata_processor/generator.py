7eec3a14caddcafafe347ace0a67e1893bfddf97
Martin Landa <landa.martin@gmail.com>
2026-03-23 07:38:55 +0100

    fix ruff

--- subsystems/dpr/metadata_processor/generator.py
+++ subsystems/dpr/metadata_processor/generator.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from typing import Optional, List, Dict, Any
-    from subsystem.qcl.logger imprt Logger
+    from subsystem.qcl.logger import Logger
     
 import json
 from datetime import datetime, UTC
