cca1d4ca627fd1fedf8e326224c577bc0b93d304
Lukas Brodsky <lukas.brodsky@natur.cuni.cz>
2026-04-02 12:53:41 +0200

    insitu import changed

--- subsystems/dag/modules/insitu/__init__.py
+++ subsystems/dag/modules/insitu/__init__.py
@@ -1,5 +1,8 @@
 
-from .loader import InSituLoader # noqa: F821 
-from .aligner import TemporalAligner # noqa: F821 
+# from .loader import InSituLoader # noqa: F821 
+# from .aligner import TemporalAligner # noqa: F821 
+
+from subsystems.dag.modules.insitu.loader import InSituLoader
+from subsystems.dag.modules.insitu.aligner import TemporalAligner
 
 __all__ = ["InSituLoader", "TemporalAligner"]
