fcf165502132183ca48040730e7fb4d3b8363d07
Martin Landa <landa.martin@gmail.com>
2026-04-21 12:12:29 +0200

    add template for water masking

---
+++ subsystems/dpr/data_analysis_pipelines/water_masking.py
@@ -0,0 +1,12 @@
+from .base import BaseDataAnalysisPipeline
+
+
+class Sentinel2WaterMaskingPipeline(BaseDataAnalysisPipeline):
+    metadata = {
+        'title': 'Sentinel-2 Water Masking',
+        'abstract': 'TBD',
+    }
+
+    def run():
+        """To be implemented."""
+        pass
