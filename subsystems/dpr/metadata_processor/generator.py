a050b9eddee1d315445e6fca4d9187a3b4b8b5b5
pesekon2 <pesej.ondrek@gmail.com>
2026-04-28 10:30:25 +0200

    DPR: MetadataGenerator - support S2
    see #154

--- subsystems/dpr/metadata_processor/generator.py
+++ subsystems/dpr/metadata_processor/generator.py
@@ -2,14 +2,15 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Optional, List, Dict, Any
+    from typing import Dict, Any
     from subsystems.qcl.logger import Logger
 
+import os
 import json
-from datetime import datetime, UTC
 from pathlib import Path
+from stactools.sentinel2.stac import create_item
 
-from osgeo import gdal, osr
+from osgeo import gdal
 
 gdal.UseExceptions()
 
@@ -27,130 +28,176 @@ class RasterDataset:
 
     def __init__(self, path: str):
         self.path = path
-        self.dataset = gdal.Open(path)
 
     @property
-    def driver(self) -> str:
-        """Returns the GDAL driver name."""
-        return self.dataset.GetDriver().ShortName
+    def stac_item(self) -> Dict[str, Any]:
+        """Get the STAC item representation for this path.
 
-    @property
-    def size(self) -> (int, int):
-        """Returns raster dimensions (width, height)."""
-        return self.dataset.RasterXSize, self.dataset.RasterYSize
-
-    @property
-    def geotransform(self) -> tuple:
-        """Returns GDAL geotransform tuple."""
-        return self.dataset.GetGeoTransform()
-
-    @property
-    def projection(self) -> str:
-        """Returns WKT projection string of the dataset."""
-        return self.dataset.GetProjection()
-
-    def get_spatial_reference(self) -> Optional[osr.SpatialReference]:
+        :return: A dictionary representing the STAC item with
         """
-        Returns SpatialReference object for the dataset if available.
+        return self.get_stac_item(self.path)
 
-        :return: SpatialReference or None
-        :rtype: osr.SpatialReference | None
+    def get_stac_item(self, path: str) -> Dict[str, Any]:
+        """Retrieve and transform a STAC item from the given path.
+
+        This method creates a standard STAC item using the stactools library,
+        then applies custom transformations to match the desired format for
+        Sentinel-2 L2A data.
+
+        :param path: The file path to the granule or product directory.
+        :return: A dictionary containing the transformed STAC item with band
+            assets, metadata assets, and updated properties.
         """
-        if not self.projection:
-            return None
-        srs = osr.SpatialReference()
-        srs.ImportFromWkt(self.projection)
-        return srs
+        # common STAC definition
+        item_standard = create_item(
+            granule_href=path,
+            additional_providers=None,
+            tolerance=None,
+            asset_href_prefix=None,
+        )
+        item_standard.set_self_href(os.path.split(path)[0])
+        # make HREFs relative; otherwise, it wouldn't be possible to test it
+        # locally (comparison would fail for HREFs)
+        item_standard.make_asset_hrefs_relative()
+        item_dict_standard = item_standard.to_dict()
 
-    def get_epsg(self) -> Optional[int]:
+        # now our tweaks
+        item_dict_transformed = self._transform_item(item_dict_standard)
+
+        return item_dict_transformed
+
+    @staticmethod
+    def _transform_item(item: Dict[str, Any]) -> Dict[str, Any]:
+        """Transform a stactools Sentinel-2 item to the desired format.
+
+        This method reshapes a standard Sentinel-2 STAC item by:
+        - Remapping band assets from semantic names (e.g., 'nir') to band
+          numbers (e.g., 'B08')
+        - Collecting EO band information as a section separate from raster:bands
+        - Updating properties with processing-level and tile information
+
+        :param item: The dictionary representation of a standard STAC item as
+            produced by stactools.
+        :return: A transformed STAC item dictionary with
+
+        TODO: Probably going to be renamed once support for S1 will be on the table
         """
-        Returns EPSG code if available.
 
-        :return: EPSG code or None
-        :rtype: int | None
-        """
-        srs = self.get_spatial_reference()
-        if srs:
-            auth = srs.GetAttrValue('AUTHORITY', 1)
-            if auth:
-                return int(auth)
-        return None
+        # Mapping from semantic band names to band numbers
+        band_mapping = {
+            'coastal': 'B01',
+            'blue': 'B02',
+            'green': 'B03',
+            'red': 'B04',
+            'rededge1': 'B05',
+            'rededge2': 'B06',
+            'rededge3': 'B07',
+            'nir': 'B08',
+            'nir08': 'B8A',
+            'nir09': 'B09',
+            # band 10 missing
+            'swir16': 'B11',
+            'swir22': 'B12',
+        }
 
-    def get_bbox_native(self) -> List[float]:
-        """
-        Returns bounding box in native dataset coordinates.
+        # create new assets dictionary with band number keys
+        new_assets = {}
+        eo_bands = []
 
-        :return: [minx, miny, maxx, maxy]
-        :rtype: list[float]
-        """
-        gt = self.geotransform
-        width, height = self.size
+        for asset_key, asset_value in item.get('assets', {}).items():
+            # metadata assets section
+            if asset_key in ['product_metadata', 'granule_metadata', 'safe_manifest']:
+                role = 'metadata'
+                if asset_key == 'product_metadata':
+                    # we want it called scene_metadata, not product_metadata
+                    new_key = 'scene_metadata'
+                else:
+                    new_key = asset_key
 
-        minx = gt[0]
-        maxy = gt[3]
-        maxx = minx + gt[1] * width
-        miny = maxy + gt[5] * height
+                new_assets[new_key] = {
+                    'href': asset_value.get('href'),
+                    'type': asset_value.get('type'),
+                    'roles': [role],
+                }
 
-        return [minx, miny, maxx, maxy]
+            # bands assets section
+            if asset_key not in band_mapping.keys():
+                continue
 
-    def get_bbox_wgs84(self) -> List[float]:
-        """
-        Returns bounding box transformed to EPSG:4326 (WGS84).
+            band_info = asset_value['eo:bands'][0]
+            band_name = band_info.get('name')
 
-        :return: [min_lon, min_lat, max_lon, max_lat]
-        :rtype: list[float]
-        """
-        minx, miny, maxx, maxy = self.get_bbox_native()
+            # Create new asset
+            new_asset = {
+                'href': asset_value.get('href'),
+                'type': asset_value.get('type', 'image/jp2'),
+                'roles': asset_value.get('roles', ['data']),
+            }
 
-        source_srs = self.get_spatial_reference()
-        if source_srs is None:
-            return [minx, miny, maxx, maxy]
+            # Use the band name as key
+            new_assets[band_name] = new_asset
 
-        target_srs = osr.SpatialReference()
-        target_srs.ImportFromEPSG(4326)
-
-        transform = osr.CoordinateTransformation(source_srs, target_srs)
-
-        corners = [
-            transform.TransformPoint(minx, miny),
-            transform.TransformPoint(minx, maxy),
-            transform.TransformPoint(maxx, maxy),
-            transform.TransformPoint(maxx, miny),
-        ]
-
-        xs = [pt[0] for pt in corners]
-        ys = [pt[1] for pt in corners]
-
-        return [min(xs), min(ys), max(xs), max(ys)]
-
-    def get_band_metadata(self) -> List[Dict[str, Any]]:
-        """
-        Returns metadata for all raster bands.
-
-        :return: List of dicts containing 'data_type' and 'nodata'
-        :rtype: list[dict[str, Any]]
-        """
-        bands = []
-        for i in range(1, self.dataset.RasterCount + 1):
-            band = self.dataset.GetRasterBand(i)
-            bands.append(
+            eo_bands.append(
                 {
-                    'data_type': gdal.GetDataTypeName(band.DataType),
-                    'nodata': band.GetNoDataValue(),
+                    'name': band_name,
+                    'common_name': band_info.get('common_name'),
+                    'center_wavelength': band_info.get('center_wavelength'),
+                    'gsd': asset_value.get('gsd'),
                 }
             )
-        return bands
+
+        # Build the new item
+        new_item = {
+            'type': item.get('type'),
+            'stac_version': '1.0.0',
+            'id': item.get('id'),
+            'bbox': item.get('bbox'),
+            'geometry': item.get('geometry'),
+            'properties': {
+                'datetime': item.get('properties', {}).get('datetime'),
+                'start_datetime': item.get('properties', {}).get('datetime'),
+                'end_datetime': item.get('properties', {}).get('datetime'),
+                'platform': item.get('properties', {}).get('platform'),
+                'constellation': item.get('properties', {}).get('constellation'),
+                'instruments': item.get('properties', {}).get('instruments'),
+                'processing:level': 'L2A',
+                'eo:cloud_cover': item.get('properties', {}).get('eo:cloud_cover'),
+                'view:sun_azimuth': item.get('properties', {}).get('view:sun_azimuth'),
+                'view:sun_elevation': item.get('properties', {}).get(
+                    'view:sun_elevation'
+                ),
+                'sat:relative_orbit': item.get('properties', {}).get(
+                    'sat:relative_orbit'
+                ),
+                's2:tile_id': item.get('properties', {}).get('grid:code').split('-')[1],
+                's2:product_uri': item.get('properties', {}).get('s2:product_uri'),
+            },
+            'assets': new_assets,
+            'collection': 'sentinel-2-l2a',
+            'links': [
+                {
+                    'rel': 'self',
+                    'href': 'https://stac.dataspace.copernicus.eu/v1/collections/sentinel-2-l2a/items/'
+                    + item.get('id'),
+                }
+            ],
+            'eo:bands': sorted(eo_bands, key=lambda x: x['name']),
+            'providers': [
+                {
+                    'name': provider.get('name'),
+                    'roles': provider.get('roles'),
+                }
+                for provider in item.get('properties', {}).get('providers', [])
+            ],
+        }
+
+        return new_item
 
 
 class StacItemFactory:
     """
     Creates STAC Item metadata from a RasterDataset.
     """
-
-    MIME_LOOKUP: Dict[str, str] = {
-        'GTiff': 'image/tiff; application=geotiff',
-        'JP2OpenJPEG': 'image/jp2',
-    }
 
     def __init__(self, raster: RasterDataset, logger: Logger):
         """
@@ -162,28 +209,6 @@ class StacItemFactory:
         self.raster = raster
         self.logger = logger
 
-    def _build_geometry(self, bbox: List[float]) -> Dict[str, Any]:
-        """
-        Creates GeoJSON Polygon for the STAC Item.
-
-        :param list[float] bbox: [minx, miny, maxx, maxy]
-
-        :return: GeoJSON Polygon
-        :rtype: dict
-        """
-        return {
-            'type': 'Polygon',
-            'coordinates': [
-                [
-                    [bbox[0], bbox[1]],
-                    [bbox[0], bbox[3]],
-                    [bbox[2], bbox[3]],
-                    [bbox[2], bbox[1]],
-                    [bbox[0], bbox[1]],
-                ]
-            ],
-        }
-
     def create_item(self) -> Dict[str, Any]:
         """
         Creates a STAC Item as a dictionary.
@@ -191,43 +216,8 @@ class StacItemFactory:
         :return: STAC Item
         :rtype: dict
         """
-        item_id = self.raster.path.stem
-        now = datetime.now(UTC).isoformat()
+        stac_item = self.raster.stac_item
 
-        bbox = self.raster.get_bbox_wgs84()
-        geometry = self._build_geometry(bbox)
-
-        width, height = self.raster.size
-
-        stac_item: Dict[str, Any] = {
-            'type': 'Feature',
-            'stac_version': '1.0.0',
-            'stac_extensions': [
-                'https://stac-extensions.github.io/projection/v1.0.0/schema.json',
-                'https://stac-extensions.github.io/raster/v1.1.0/schema.json',
-            ],
-            'id': item_id,
-            'collection': 'undefined',
-            'properties': {
-                'datetime': now,
-                'proj:epsg': self.raster.get_epsg(),
-                'proj:shape': [height, width],
-                'proj:transform': list(self.raster.geotransform),
-                'raster:bands': self.raster.get_band_metadata(),
-            },
-            'bbox': bbox,
-            'geometry': geometry,
-            'assets': {
-                'data': {
-                    'href': self.raster.path.name,
-                    'type': self.MIME_LOOKUP.get(
-                        self.raster.driver, 'application/octet-stream'
-                    ),
-                    'roles': ['data'],
-                }
-            },
-            'links': [],
-        }
         self.logger.debug(f'STAC item created: {stac_item}')
 
         return stac_item
@@ -263,18 +253,13 @@ class MetadataGenerator(GaiaBase):
 
         :param str data_source: path to the datasource (raster, tabular data...)
         """
-        # TODO: allowed file extensions should be part of internal settings (see #89)
-        # TODO: do we want to rely on file extension only?
-        if Path(data_source).suffix in ('.tif', '.jp2'):
-            self.logger.info(f'Metadata generator datasource: {data_source}')
-            self._ds = RasterDataset(data_source)
-            self._factory = StacItemFactory(self._ds, self.logger)
-        elif Path(data_source).suffix in ('.csv',):
+        if Path(data_source).suffix in ('.csv',):
             # TODO: ISU
             pass
         else:
-            # TODO: replace by GAIA-TSF exception
-            raise RuntimeError('Unsupported datasource')
+            self.logger.info(f'Metadata generator datasource: {data_source}')
+            self._ds = RasterDataset(data_source)
+            self._factory = StacItemFactory(self._ds, self.logger)
 
     @property
     def stac(self) -> StacItemFactory:
--- subsystems/dpr/tests/test_modules.py
+++ subsystems/dpr/tests/test_modules.py
@@ -1,6 +1,10 @@
 import json
+import os
+import zipfile
 
 from pathlib import Path
+from subsystems.eou.data_acquisition_gateway import DataAcquisitionGateway
+from lib.config import ProjectConfigReader
 
 from subsystems.dpr.metadata_processor import MetadataGenerator
 from subsystems.dpr.preprocessing_pipelines import PreprocessingPipelines
@@ -63,31 +67,56 @@ class TestModules:
         """
         pass
 
-    def test_MetadataProcessor_001(self):
+    def test_MetadataProcessor_001(self, tmp_path):
         """Test MetadataProcessor module.
 
         Generate data-driven metadata using MetadataGenerator for
         raster-based datasource.
         """
-
         def item_dict_no_datetime(item_dict):
             if 'properties' in item_dict and 'datetime' in item_dict['properties']:
                 del item_dict['properties']['datetime']
             return item_dict
 
-        module = MetadataGenerator()
-        module.set_datasource(
-            Path(__file__).parent.parent.parent
-            / 'eou'
+        # first, we need to download the S2 product
+        search_filter = {
+            'provider': 'cop_dataspace',
+            'start': '2026-01-01',
+            'end': '2026-01-29',
+            'productType': 'S2_MSI_L2A',
+        }
+
+        config = ProjectConfigReader(
+            Path(__file__).parent.parent.parent.parent
             / 'tests'
-            / 'sample_data'
-            / ('ENMAP01_sample.tif')
+            / 'projects'
+            / 'jagersfontein.yml'
         )
+
+        dag = DataAcquisitionGateway()
+
+        results = dag.backend.search(
+            geom=config['project']['aoi']['geom'],
+            **search_filter,
+        )
+
+        # S2 product must be extracted for stactools
+        s2_path = dag.backend.download(results[0], target_dir=tmp_path)
+        with zipfile.ZipFile(s2_path, 'r') as zip_ref:
+            zip_ref.extractall(tmp_path)
+
+        # finally, let's generate the metadata
+        module = MetadataGenerator()
+        module.set_datasource(os.path.splitext(s2_path)[0] + '.SAFE')
         item_dict = module.stac.create_item()
 
         with open(
-            Path(__file__).parent / 'sample_data' / 'ENMAP01_sample.json',
+            Path(__file__).parent
+            / 'sample_data'
+            / 'S2B_MSIL2A_20260103T080229_N0511_R035_T35JLH_20260103T103434.json',
             'r',
         ) as f:
             json_dict = json.load(f)
-        assert item_dict_no_datetime(item_dict) == item_dict_no_datetime(json_dict)
+        assert item_dict_no_datetime(
+            json.loads(json.dumps(item_dict))
+        ) == item_dict_no_datetime(json_dict)
