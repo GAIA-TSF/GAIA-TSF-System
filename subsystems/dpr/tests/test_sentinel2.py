6f48e99f28f4411f8b0525276c1ce051ab0a6400
l-andreani <andreani.louis@googlemail.com>
2026-05-04 15:49:42 +0200

    DPR: Sentinel-2 SAFE Processor (#218)
    First draft of the pipeline. 
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Martin Landa <landa.martin@gmail.com>
    Co-authored-by: Ondrej Pesek <pesej.ondrek@gmail.com>

--- subsystems/dpr/tests/test_sentinel2.py
+++ subsystems/dpr/tests/test_sentinel2.py
@@ -1,11 +1,148 @@
 import json
 import os
+import shutil
+from pathlib import Path
 
+import numpy
+from osgeo import gdal
+from shapely import wkt
+from shapely.ops import transform
+from pyproj import Transformer
+
+from subsystems.eou.data_acquisition_gateway import DataAcquisitionGateway
 from subsystems.dpr.preprocessing_pipelines import Sentinel2CloudCoverPipeline
+from subsystems.dpr.preprocessing_pipelines import Sentinel2SafeProcessor
 
 
 class TestSentinel2Workflow:
-    def test_cloudcover(self):
+    def test_sentinel2_safe_processor(self):
+        """Test the Sentinel2SafeProcessor."""
+        # GDAL configuration to handle errors
+        gdal.UseExceptions()
+
+        # Retrieve a Sentinel-2 safe product using EOU's DataAcquisitionGateway
+        dag_module = DataAcquisitionGateway()
+
+        # Set ROI as a WKT string (WGS84). This will be used for both product search and cropping parameter.
+        roi = 'POLYGON((14.757 60.027, 14.757 60.052, 14.826 60.052, 14.826 60.027, 14.757 60.027))'
+
+        # Set search filters
+        search_filter = {
+            'provider': 'cop_dataspace',
+            'start': '2018-07-01',
+            'end': '2018-07-31',
+            'productType': 'S2_MSI_L2A',
+        }
+
+        results = dag_module.backend.search(
+            geom=roi,
+            **search_filter,
+        )
+        data_path = dag_module.backend.download(
+            results[0], quicklook=False, target_dir='sentinel2'
+        )
+
+        # Check if the SAFE product can be located
+        assert os.path.exists(data_path), 'The SAFE product was not found.'
+
+        # Define an output folder to store the results
+        output_folder = os.path.abspath(
+            'subsystems/dpr/tests/sample_data/output_safe_processor'
+        )
+
+        # Delete any pre-existing outputs
+        if os.path.exists(output_folder):
+            shutil.rmtree(output_folder)
+
+        # Set output resolution
+        res = (10, 10)
+
+        # Set resampling algorithm
+        r_alg = 'bilinear'
+
+        # Run the pipeline
+        pipeline = Sentinel2SafeProcessor()
+        pipeline.configure(
+            input_safe=Path(data_path),
+            output_folder=Path(output_folder),
+            roi=roi,
+            target_res=res,
+            resampling_alg=r_alg,
+            overwrite=True,
+        )
+        pipeline.run()
+
+        # Check if the files were created
+        filename = (
+            os.path.basename(data_path).replace('.SAFE', '').replace('.zip', '')
+            + '.tiff'
+        )
+        tiff_path = os.path.join(output_folder, filename)
+        assert os.path.exists(tiff_path), f'The Geotiff was not created: {tiff_path}'
+        json_path = os.path.join(output_folder, tiff_path.replace('.tiff', '.json'))
+        assert os.path.exists(json_path), 'The metadata file was not created.'
+
+        # Check if essential keys are present in the metadata file
+        with open(json_path, 'r') as f:
+            metadata = json.load(f)
+        essential_keys = [
+            'PRODUCT_START_TIME',
+            'PRODUCT_URI',
+            'PROCESSING_LEVEL',
+            'PRODUCT_TYPE',
+            'DATATAKE_SENSING_START',
+            'Wavelengths',
+            'Reflectance_Conversion',
+            'HORIZONTAL_CS_NAME',
+            'HORIZONTAL_CS_CODE',
+            'source_path',
+            'Input_SAFE_path',
+        ]
+        for key in essential_keys:
+            assert key in metadata, f'Key {key} is missing from metadata file.'
+
+        # Checks if Geotiff was produced with all the bands and according to inputs
+        ds = gdal.Open(tiff_path, gdal.GA_Update)
+        assert ds.RasterCount == 13, (
+            'The Geotiff does not contain the right amount of bands.'
+        )
+
+        # Check if output resolution is same as input
+        gt = ds.GetGeoTransform()
+        output_res = (numpy.abs(gt[1]), numpy.abs(gt[5]))
+        assert output_res == res, (
+            f'The spatial resolution of the Geotiff {output_res} '
+            f'differs from the input parameters {res} .'
+        )
+
+        # Check if output ROI is same as input ROI
+        # Note: Practically we check if the difference between the bounding boxes coordinates are less than the
+        # resolution of 1 pixel (e.g. less or equal to 10m is the input spatial resolution is 10m)
+        # Output ROI:
+        width = ds.RasterXSize
+        height = ds.RasterYSize
+        minx = gt[0]
+        maxy = gt[3]
+        maxx = minx + gt[1] * width
+        miny = maxy + gt[5] * height
+        output_roi = numpy.array((minx, miny, maxx, maxy))
+        # input ROI:
+        geom = wkt.loads(roi)
+        target_epsg = metadata['HORIZONTAL_CS_CODE']
+        transformer = Transformer.from_crs('EPSG:4326', target_epsg, always_xy=True)
+        transformed_geom = transform(transformer.transform, geom)
+        input_roi = numpy.array(transformed_geom.bounds)
+        # Verify that the offsets are less than the pixel resolution
+        offsets = numpy.abs(input_roi - output_roi)
+        assert numpy.nanmax(offsets) < numpy.nanmin(res), (
+            f'The spatial extent of the Geotiff {output_roi} '
+            f'differs by more than {numpy.nanmin(res)} meters from the input parameters {input_roi} .'
+        )
+
+        # Delete folder after test is complete
+        shutil.rmtree(output_folder)
+
+    def test_sentinel2_cloudcover(self):
         """Test the Sentinel2CloudCoverPipeline."""
 
         # Get path for sentinel-2 sample scene and create a path for test metadata
