GAIA-TSF/GAIA-TSF-System

commit 2f1c0a4b45bdaef929ada687a905d40ef47a31c7

class params - initial commit

subsystems/dpr/preprocessing_pipelines/sentinel1.py
@@ -15,11 +15,15 @@ class Sentinel1Pipeline(PreprocessingBasePipeline):
     metadata = {
         'title': 'Sentinel-1',
         'abstract': 'Anomaly detection for slope stability: preprocess Sentinel-1 data',
-        'params': {},
+        'params': {
+            'aoi': {
+                dtype: str,
+                'description': 'POLYGON wkt string, coordinates must be in WGS84 (EPSG:4326)',
+            }
+        },
     }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def _configure(self):
         self.client = None
         self.s1 = None
         self.stack = None
@@ -428,6 +432,5 @@ class Sentinel1Pipeline(PreprocessingBasePipeline):
         )
 
     def _run(self):
-        self._build_sbas_stack()
-        self._reframe_sbas
-        # ...
+        # self._download_dem(self.aoi, ...)
+        pass

subsystems/dpr/tests/test_sentinel1.py
@@ -484,4 +484,6 @@ class TestSentinel1Workflow:
                 assert high_risk_areas.min(skipna=True) >= 6
 
     def test_run_workflow(self, pipeline, config):
+        # pipeline.configure('WKT...', ...)
+        # pipeline.run()
         pass

commit 261e5942855a45c421f8e3761384f05637fe2181

fix syntax typo introduced by 2f1c0a4

subsystems/dpr/preprocessing_pipelines/sentinel1.py
@@ -17,7 +17,7 @@ class Sentinel1Pipeline(PreprocessingBasePipeline):
         'abstract': 'Anomaly detection for slope stability: preprocess Sentinel-1 data',
         'params': {
             'aoi': {
-                dtype: str,
+                'dtype': str,
                 'description': 'POLYGON wkt string, coordinates must be in WGS84 (EPSG:4326)',
             }
         },