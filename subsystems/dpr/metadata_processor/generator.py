1e33cb715e57604f8cd5223dc4f745ebb0ec1d35
pesekon2 <pesej.ondrek@gmail.com>
2026-05-07 11:42:40 +0200

    return RasterDataset

--- subsystems/dpr/metadata_processor/generator.py
+++ subsystems/dpr/metadata_processor/generator.py
@@ -28,6 +28,132 @@ class RasterDataset:
 
     def __init__(self, path: str):
         self.path = path
+        self.dataset = gdal.Open(path)
+
+    @property
+    def driver(self) -> str:
+        """Returns the GDAL driver name."""
+        return self.dataset.GetDriver().ShortName
+
+    @property
+    def size(self) -> (int, int):
+        """Returns raster dimensions (width, height)."""
+        return self.dataset.RasterXSize, self.dataset.RasterYSize
+
+    @property
+    def geotransform(self) -> tuple:
+        """Returns GDAL geotransform tuple."""
+        return self.dataset.GetGeoTransform()
+
+    @property
+    def projection(self) -> str:
+        """Returns WKT projection string of the dataset."""
+        return self.dataset.GetProjection()
+
+    def get_spatial_reference(self) -> Optional[osr.SpatialReference]:
+        """
+        Returns SpatialReference object for the dataset if available.
+
+        :return: SpatialReference or None
+        :rtype: osr.SpatialReference | None
+        """
+        if not self.projection:
+            return None
+        srs = osr.SpatialReference()
+        srs.ImportFromWkt(self.projection)
+        return srs
+
+    def get_epsg(self) -> Optional[int]:
+        """
+        Returns EPSG code if available.
+
+        :return: EPSG code or None
+        :rtype: int | None
+        """
+        srs = self.get_spatial_reference()
+        if srs:
+            auth = srs.GetAttrValue('AUTHORITY', 1)
+            if auth:
+                return int(auth)
+        return None
+
+    def get_bbox_native(self) -> List[float]:
+        """
+        Returns bounding box in native dataset coordinates.
+
+        :return: [minx, miny, maxx, maxy]
+        :rtype: list[float]
+        """
+        gt = self.geotransform
+        width, height = self.size
+
+        minx = gt[0]
+        maxy = gt[3]
+        maxx = minx + gt[1] * width
+        miny = maxy + gt[5] * height
+
+        return [minx, miny, maxx, maxy]
+
+    def get_bbox_wgs84(self) -> List[float]:
+        """
+        Returns bounding box transformed to EPSG:4326 (WGS84).
+
+        :return: [min_lon, min_lat, max_lon, max_lat]
+        :rtype: list[float]
+        """
+        minx, miny, maxx, maxy = self.get_bbox_native()
+
+        source_srs = self.get_spatial_reference()
+        if source_srs is None:
+            return [minx, miny, maxx, maxy]
+
+        target_srs = osr.SpatialReference()
+        target_srs.ImportFromEPSG(4326)
+
+        transform = osr.CoordinateTransformation(source_srs, target_srs)
+
+        corners = [
+            transform.TransformPoint(minx, miny),
+            transform.TransformPoint(minx, maxy),
+            transform.TransformPoint(maxx, maxy),
+            transform.TransformPoint(maxx, miny),
+        ]
+
+        xs = [pt[0] for pt in corners]
+        ys = [pt[1] for pt in corners]
+
+        return [min(xs), min(ys), max(xs), max(ys)]
+
+    def get_band_metadata(self) -> List[Dict[str, Any]]:
+        """
+        Returns metadata for all raster bands.
+
+        :return: List of dicts containing 'data_type' and 'nodata'
+        :rtype: list[dict[str, Any]]
+        """
+        bands = []
+        for i in range(1, self.dataset.RasterCount + 1):
+            band = self.dataset.GetRasterBand(i)
+            bands.append(
+                {
+                    'data_type': gdal.GetDataTypeName(band.DataType),
+                    'nodata': band.GetNoDataValue(),
+                }
+            )
+        return bands
+
+
+class SentinelDataset:
+    """
+    GDAL dataset wrapper for extracting spatial and raster metadata.
+
+    :param str path: Path to the raster data source
+
+    :raises RuntimeError: If the file cannot be opened.
+    """
+
+    def __init__(self, path: str):
+        self.path = path
 
     @property
     def stac_item(self) -> Dict[str, Any]:
@@ -385,13 +511,15 @@ class MetadataGenerator(GaiaBase):
 
         :param str data_source: path to the datasource (raster, tabular data...)
         """
+        self.logger.info(f'Metadata generator datasource: {data_source}')
         if Path(data_source).suffix in ('.csv',):
-            self.logger.info(f'Metadata generator datasource: {data_source}')
             self._ds = InsituDataset(data_source, self._isu_metadata)
             self._factory = InsituStacItemFactory(self._ds, self.logger)
-        else:
-            self.logger.info(f'Metadata generator datasource: {data_source}')
+        elif Path(data_source).suffix in ('.tif', '.jp2'):
             self._ds = RasterDataset(data_source)
+            self._factory = StacItemFactory(self._ds, self.logger)
+        else:
+            self._ds = SentinelDataset(data_source)
             self._factory = StacItemFactory(self._ds, self.logger)
 
     @property
--- subsystems/dpr/tests/test_modules.py
+++ subsystems/dpr/tests/test_modules.py
@@ -67,7 +67,35 @@ class TestModules:
         """
         pass
 
-    def test_MetadataProcessor_001(self, tmp_path):
+    def test_MetadataProcessor_001(self):
+        """Test MetadataProcessor module.
+
+        Generate data-driven metadata using MetadataGenerator for
+        raster-based datasource.
+        """
+        def item_dict_no_datetime(item_dict):
+            if 'properties' in item_dict and 'datetime' in item_dict['properties']:
+                del item_dict['properties']['datetime']
+            return item_dict
+
+        module = MetadataGenerator()
+        module.set_datasource(
+            Path(__file__).parent.parent.parent
+            / 'eou'
+            / 'tests'
+            / 'sample_data'
+            / ('ENMAP01_sample.tif')
+        )
+        item_dict = module.stac.create_item()
+
+        with open(
+            Path(__file__).parent / 'sample_data' / 'ENMAP01_sample.json',
+            'r',
+        ) as f:
+            json_dict = json.load(f)
+        assert item_dict_no_datetime(item_dict) == item_dict_no_datetime(json_dict)
+
+    def test_MetadataProcessor_002(self, tmp_path):
         """Test MetadataProcessor module.
 
         Generate data-driven metadata using MetadataGenerator for
