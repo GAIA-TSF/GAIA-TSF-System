a7d5a16f8f4ddfb574e9ccabd20829487f2b541a
Martin Landa <landa.martin@gmail.com>
2026-06-19 21:07:08 +0200

    add dpr_sentinel1_workflow + eou_download_sentinel2

---
+++ examples/eou_download_sentinel2.py
@@ -0,0 +1,23 @@
+from subsystems.eou.data_acquisition_gateway import DataAcquisitionGateway
+from lib.config import ProjectConfigReader
+from tests.utils import TestUtils
+
+project_config = ProjectConfigReader(
+    TestUtils.get_project_config_path('amd_monitoring_yxsjoberg')
+)
+
+dag_module = DataAcquisitionGateway(backend='eodag')
+
+search_filter = {
+    'provider': 'cop_dataspace',
+    'start': '2025-06-01',
+    'end': '2025-06-05',
+    'productType': 'S2_MSI_L2A',
+}
+
+results = dag_module.backend.search(
+    geom=project_config.aoi(),
+    **search_filter,
+)
+data_path = dag_module.backend.download_all(results, target_dir='sentinel2', quicklook=False)
+
