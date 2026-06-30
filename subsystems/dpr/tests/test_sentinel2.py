69f9328ade70abf41a2e866bbb3d280146f83f5c
l-andreani <andreani.louis@googlemail.com>
2026-06-30 12:47:09 +0200

    Update test_sentinel2.py
    Changed threshold for bboxes offset test pass to 60m

--- subsystems/dpr/tests/test_sentinel2.py
+++ subsystems/dpr/tests/test_sentinel2.py
@@ -168,9 +168,10 @@ class TestSentinel2Workflow:
 
         # Check if metadata bbox is same as input ROI
         output_bbox = numpy.array(metadata['bbox'])
-        input_bbox = numpy.array(ogr.CreateGeometryFromWkt(roi).GetEnvelope())
-        offsets = numpy.abs(input_roi - output_roi)
-        res_dd = numpy.nanmin(res) / 111320.0  # very rough conversion from meters to dd
+        xmin, xmax, ymin, ymax = ogr.CreateGeometryFromWkt(roi).GetEnvelope()
+        input_bbox = numpy.array((xmin, ymin, xmax, ymax))
+        offsets = numpy.abs(input_bbox - output_bbox)
+        res_dd = 60. / 111320.  # rough conversion of 60m to dd
         assert numpy.nanmax(offsets) < numpy.nanmin(res_dd), (
             f'The bounding box of the output raster {output_bbox} '
             f'differs by more than {numpy.nanmin(res)} meters from the input bounding box {input_bbox} .'
@@ -333,7 +334,7 @@ class TestSentinel2Workflow:
         xmin, xmax, ymin, ymax = ogr.CreateGeometryFromWkt(roi).GetEnvelope()
         input_bbox = numpy.array((xmin, ymin, xmax, ymax))
         offsets = numpy.abs(input_bbox - output_bbox)
-        res_dd = numpy.nanmin(res) / 111320.0  # very rough conversion from meters to dd
+        res_dd = 60. / 111320.  # rough conversion of 60m to dd
         assert numpy.nanmax(offsets) < numpy.nanmin(res_dd), (
             f'The bounding box of the output raster {output_bbox} '
             f'differs by more than {numpy.nanmin(res)} meters from the input bounding box {input_bbox} .'
