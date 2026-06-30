b2b7179ca2e152dd0622cd2aacceee22a758b393
l-andreani <177834616+l-andreani@users.noreply.github.com>
2026-06-30 08:19:13 +0000

    Apply Ruff formatting and fixes

--- subsystems/dpr/tests/test_sentinel2.py
+++ subsystems/dpr/tests/test_sentinel2.py
@@ -170,7 +170,7 @@ class TestSentinel2Workflow:
         output_bbox = numpy.array(metadata['bbox'])
         input_bbox = numpy.array(ogr.CreateGeometryFromWkt(roi).GetEnvelope())
         offsets = numpy.abs(input_roi - output_roi)
-        res_dd = numpy.nanmin(res)/111320.0  # very rough conversion from meters to dd
+        res_dd = numpy.nanmin(res) / 111320.0  # very rough conversion from meters to dd
         assert numpy.nanmax(offsets) < numpy.nanmin(res_dd), (
             f'The bounding box of the output raster {output_bbox} '
             f'differs by more than {numpy.nanmin(res)} meters from the input bounding box {input_bbox} .'
@@ -332,7 +332,7 @@ class TestSentinel2Workflow:
         output_bbox = numpy.array(metadata['bbox'])
         input_bbox = numpy.array(ogr.CreateGeometryFromWkt(roi).GetEnvelope())
         offsets = numpy.abs(input_roi - output_roi)
-        res_dd = numpy.nanmin(res)/111320.0  # very rough conversion from meters to dd
+        res_dd = numpy.nanmin(res) / 111320.0  # very rough conversion from meters to dd
         assert numpy.nanmax(offsets) < numpy.nanmin(res_dd), (
             f'The bounding box of the output raster {output_bbox} '
             f'differs by more than {numpy.nanmin(res)} meters from the input bounding box {input_bbox} .'
